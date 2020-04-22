13de1b7f1147b195eb5c7468aa50d4e88d5121d0
Matt McKay <mamckay@gmail.com>
2020-04-22 16:40:37 +1000

    add debug

--- scripts/build-website.sh
+++ scripts/build-website.sh
@@ -14,6 +14,8 @@ echo "List of Changed RST Files: $RST_FILES"
 if [ -z "$RST_FILES" ]; then
     echo "No RST Files have changed -- nothing to do in this PR"
 else
+    pwd
     RST_FILES="$RST_FILES source/rst/index_toc.rst"
     make website FILES="$RST_FILES" THEMEPATH="theme/lectures-python-programming.theme"
+    ls _build/website/jupyter_html/*
 fi
