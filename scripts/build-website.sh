dec26282314cbd12d70c0075e21880faecee1b0d
Matt McKay <mamckay@gmail.com>
2020-04-22 20:05:33 +1000

    fix typo

--- scripts/build-website.sh
+++ scripts/build-website.sh
@@ -14,9 +14,8 @@ echo "List of Changed RST Files: $RST_FILES"
 if [ -z "$RST_FILES" ]; then
     echo "No RST Files have changed -- nothing to do in this PR"
 else
-    #https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=909728
-    rm -rf /usr/local/share/fonts
-    ls theme/lectures-python-programming.theme/*
+    echo "Working Directory: $pwd"
+    ls theme/lecture-python-programming.theme/*
     RST_FILES="$RST_FILES source/rst/index_toc.rst"
     FILES="$RST_FILES"
     make website THEMEPATH=theme/lectures-python-programming.theme
