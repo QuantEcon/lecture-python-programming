b5e0ab68d2704cc6f570c801517cefeb4fc71462
Matt McKay <mamckay@gmail.com>
2020-07-27 10:55:03 +1000

    add rst2myst extension

--- conf.py
+++ conf.py
@@ -40,6 +40,7 @@ extensions = [
 	'IPython.sphinxext.ipython_console_highlighting',
     # Custom Sphinx Extensions
     'sphinxcontrib.jupyter', 
+    'sphinxcontrib.rst2myst',
 ]
 
 # Retired Extensions but may be useful in Future
